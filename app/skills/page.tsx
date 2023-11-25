8bb708813013b0bb1a8f44e0339e4224a058f177
Xavier Andueza <xand0001@student.monash.edu>
2023-11-25 21:21:25 +1100

    Taking code from the next.js dashboard tutorial
    Tutorial link here: https://nextjs.org/learn/dashboard-app

---
+++ app/skills/page.tsx
@@ -0,0 +1,40 @@
+import Pagination from '../ui/skills/pagination';
+import Search from '../ui/search';
+import Table from '../ui/skills/table';
+import { CreateInvoice } from '../ui/skills/buttons'; 
+import { lusitana } from '../ui/fonts'; 
+import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '../ui/skeletons';
+import { fetchInvoicesPages } from '../lib/data';
+ 
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await fetchInvoicesPages(query);
+ 
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
+}
