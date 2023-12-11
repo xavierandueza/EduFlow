4062b6c6063d6e08bc9518a20da6eea8d969ec14
Xavier Andueza <xand0001@student.monash.edu>
2023-12-11 14:19:53 +1100

    Connected to firestore
    Connected and verified success of connection to firestore.

---
+++ app/InteractiveButton.client.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { db } from './firebase';
+import { collection, addDoc } from "firebase/firestore";
+
+const InteractiveButton = () => {
+  const addDataToDB = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
+  return (
+    <button
+      onClick={addDataToDB}
+      className="rounded-lg bg-388a91 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-17363f md:text-base"
+    >
+      Add New Info
+    </button>
+  );
+};
+
+export default InteractiveButton;
---
+++ app/firebase.ts
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA6HxjXsl347lsFWyiC6_dRt2Va0TWijPw",
+  authDomain: "eduflowau.firebaseapp.com",
+  projectId: "eduflowau",
+  storageBucket: "eduflowau.appspot.com",
+  messagingSenderId: "911044059845",
+  appId: "1:911044059845:web:1355b18ee0390c2b7886ac",
+  measurementId: "G-RD7QKVQ21X"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+export const db = getFirestore(app);
