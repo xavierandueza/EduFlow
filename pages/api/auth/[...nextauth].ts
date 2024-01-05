3b99f7afd8b63fb7fa9f469e0fd011f327b636b6
Xavier Andueza <xand0001@student.monash.edu>
2024-01-05 22:10:06 +1100

    Revert "Revert "reintroduced the rest, emailprovider may be the issue""
    This reverts commit 70b7c34cfbec6fb6a4ac25c878d85879df75248f.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -13,11 +13,76 @@ import { db } from "../../../app/firebase";
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+          stripeCustomerId: null, // payments
+          subscriptionActive: false, // payments
+          subscriptionName: null, // payments
+          firstName: null,
+          lastName: null,
+          role: null, // parent, student, teacher
+        };
+      },
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
       allowDangerousEmailAccountLinking: true,
     }),
   ],
+  callbacks: {
+    session({
+      session,
+      token,
+      user,
+    }: {
+      session: Session;
+      token: JWT;
+      user: AdapterUser;
+    }) {
+      session.user.id = user.id;
+      session.user.firstName = user.firstName;
+      session.user.lastName = user.lastName;
+      session.user.role = user.role;
+      session.user.stripeCustomerId = user.stripeCustomerId;
+      session.user.subscriptionActive = user.subscriptionActive;
+      return session;
+    },
+  },
+  events: {
+    createUser: async ({ user }) => {
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+        apiVersion: "2023-10-16",
+      });
+
+      await stripe.customers
+        .create({
+          email: user.email!, // this is subject to change through account
+          name: user.name!,
+        })
+        .then(async (customer) => {
+          return await setDoc(
+            doc(db, "users", user.id),
+            {
+              stripeCustomerId: customer.id,
+            },
+            { merge: true }
+          );
+        });
+    },
+  },
+  adapter: FirestoreAdapter(firestore),
+  session: {
+    strategy: "database", // use database, which means it looks up in database using a hashed code value. Otherwise uses JWT
+    maxAge: 30 * 24 * 60 * 60, // in seconds, so 14 days here
+    updateAge: 24 * 60 * 60, // How frequently to update database for sessions, seconds again
+    // generateSessionToken // If we want to generate our own session token, we don't care tho
+  },
+  pages: {
+    newUser: "/account/signup", // New users will be directed here on first sign in (leave the property out if not of interest)
+  },
 };
 
 export default NextAuth(authOptions);
