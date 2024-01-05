4dddf72f1fabcc163d0170d72b09df254b0e5dbf
Xavier Andueza <xand0001@student.monash.edu>
2024-01-05 21:50:25 +1100

    Removed session from database

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -66,12 +66,6 @@ export const authOptions: NextAuthOptions = {
     },
   },
   adapter: FirestoreAdapter(firestore),
-  session: {
-    strategy: "database", // use database, which means it looks up in database using a hashed code value. Otherwise uses JWT
-    maxAge: 30 * 24 * 60 * 60, // in seconds, so 14 days here
-    updateAge: 24 * 60 * 60, // How frequently to update database for sessions, seconds again
-    // generateSessionToken // If we want to generate our own session token, we don't care tho
-  },
   pages: {
     newUser: "/account/signup", // New users will be directed here on first sign in (leave the property out if not of interest)
   },
