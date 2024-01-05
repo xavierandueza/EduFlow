99f99df04023e455f6ee8378999d603675ad90b1
Xavier Andueza <xand0001@student.monash.edu>
2024-01-05 22:11:15 +1100

    removed the extra session stuff

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -13,44 +13,11 @@ import { db } from "../../../app/firebase";
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      profile(profile) {
-        return {
-          id: profile.sub,
-          name: profile.name,
-          email: profile.email,
-          image: profile.picture,
-          stripeCustomerId: null, // payments
-          subscriptionActive: false, // payments
-          subscriptionName: null, // payments
-          firstName: null,
-          lastName: null,
-          role: null, // parent, student, teacher
-        };
-      },
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
       allowDangerousEmailAccountLinking: true,
     }),
   ],
-  callbacks: {
-    session({
-      session,
-      token,
-      user,
-    }: {
-      session: Session;
-      token: JWT;
-      user: AdapterUser;
-    }) {
-      session.user.id = user.id;
-      session.user.firstName = user.firstName;
-      session.user.lastName = user.lastName;
-      session.user.role = user.role;
-      session.user.stripeCustomerId = user.stripeCustomerId;
-      session.user.subscriptionActive = user.subscriptionActive;
-      return session;
-    },
-  },
   events: {
     createUser: async ({ user }) => {
       const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
