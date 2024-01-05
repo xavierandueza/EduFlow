d8e49ba143aee47f8eece9da43a9a9c70287bfe9
Xavier Andueza <xand0001@student.monash.edu>
2024-01-05 22:14:27 +1100

    removed the events call

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -13,31 +13,42 @@ import { db } from "../../../app/firebase";
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+          stripeCustomerId: null, // payments
+          subscriptionActive: false, // payments
+          subscriptionName: null, // payments
+          firstName: null,
+          lastName: null,
+          role: null, // parent, student, teacher
+        };
+      },
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
       allowDangerousEmailAccountLinking: true,
     }),
   ],
-  events: {
-    createUser: async ({ user }) => {
-      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-        apiVersion: "2023-10-16",
-      });
-
-      await stripe.customers
-        .create({
-          email: user.email!, // this is subject to change through account
-          name: user.name!,
-        })
-        .then(async (customer) => {
-          return await setDoc(
-            doc(db, "users", user.id),
-            {
-              stripeCustomerId: customer.id,
-            },
-            { merge: true }
-          );
-        });
+  callbacks: {
+    session({
+      session,
+      token,
+      user,
+    }: {
+      session: Session;
+      token: JWT;
+      user: AdapterUser;
+    }) {
+      session.user.id = user.id;
+      session.user.firstName = user.firstName;
+      session.user.lastName = user.lastName;
+      session.user.role = user.role;
+      session.user.stripeCustomerId = user.stripeCustomerId;
+      session.user.subscriptionActive = user.subscriptionActive;
+      return session;
     },
   },
   adapter: FirestoreAdapter(firestore),
