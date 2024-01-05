410acbfc6248898be064d6c7f5f1c3ab653322b2
Xavier Andueza <xand0001@student.monash.edu>
2024-01-05 23:02:21 +1100

    Revert "removed email provider"
    This reverts commit 9582109018cdd1fbd9586e3da89745b1462bb5b3.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -12,6 +12,17 @@ import { db } from "../../../app/firebase";
 
 export const authOptions: NextAuthOptions = {
   providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
     GoogleProvider({
       profile(profile) {
         return {
